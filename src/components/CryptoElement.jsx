256fbba3d8fcb235cb6ce2909193b45df5ccf7fc
Josh Walters <joshwalters049@gmail.com>
2023-11-17 15:18:54 +0000

    Added Loading state

--- src/components/CryptoElement.jsx
+++ src/components/CryptoElement.jsx
@@ -12,7 +12,7 @@ const CryptoElement = ({ name, price, marketcap, pricechange, image, symbol }) =
                 <div className={classes.name}><img src={image}></img> <p>{name}</p> <p>({symbol})</p></div>
                 <div className={classes.price}>{price}</div>
                 <div className={classes.marketcap}>{marketcap}</div>
-                <div style={{ color: change >= 0 ? 'green' : 'red' }} className={classes.pricechange}>{pricechange}%</div>
+                <div style={{ color: change >= 0 ? '#2ecc71' : '#e74c3c' }} className={classes.pricechange}>{pricechange}%</div>
             </li>
         </>
     )
--- src/components/CryptoList.jsx
+++ src/components/CryptoList.jsx
@@ -4,15 +4,6 @@ import CryptoElement from './CryptoElement.jsx';
 
 import classes from './CryptoList.module.css'
 
-
-const DUMMY_DATA = [
-    { name: 'Bitcoin', price: '$30000', marketcap: '$20000000', circulatingsupply: '$3000000', id: Math.random() * 1000 },
-    { name: 'Ethereum', price: '$30000', marketcap: '$20000000', circulatingsupply: '$3000000', id: Math.random() * 1000 },
-    { name: 'Solana', price: '$30000', marketcap: '$20000000', circulatingsupply: '$3000000', id: Math.random() * 1000 },
-    { name: 'Cardano', price: '$30000', marketcap: '$20000000', circulatingsupply: '$3000000', id: Math.random() * 1000 },
-    { name: 'Link', price: '$30000', marketcap: '$20000000', circulatingsupply: '$3000000', id: Math.random() * 1000 },
-]
-
 const options = {
     method: 'GET',
     headers: {
@@ -23,16 +14,19 @@ const options = {
 const CryptoList = () => {
 
     const [topCryptos, setTopCryptos] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     const cryptoFetcher = async () => {
         try {
-            const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en`, options);
+            setIsLoading(true)
+            const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en`, options);
             if (!response.ok) {
                 throw new Error('Perhaps the API is down! Please try again later.');
             }
             const data = await response.json();
             setTopCryptos(data);
             console.log(data);
+            setIsLoading(false);
         }
 
         catch (error) {
@@ -46,9 +40,12 @@ const CryptoList = () => {
 
 
     return (
-        <ul className={classes.cryptoList}>
-            {topCryptos.map((element) => <CryptoElement name={element.name} image={element.image} symbol={element.symbol} price={`${element.current_price.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`} marketcap={element.market_cap.toLocaleString('en-US', { style: 'currency', currency: 'USD' })} pricechange={element.price_change_percentage_24h.toFixed(1)} key={element.id}></CryptoElement>)}
-        </ul>
+        <>
+            {isLoading && <h2>Loading...</h2>}
+            {!isLoading && <ul className={classes.cryptoList}>
+                {topCryptos.map((element) => <CryptoElement name={element.name} image={element.image} symbol={element.symbol} price={`${element.current_price.toLocaleString('en-US', { style: 'currency', currency: 'GBP' })}`} marketcap={element.market_cap.toLocaleString('en-US', { style: 'currency', currency: 'GBP' })} pricechange={element.price_change_percentage_24h.toFixed(1)} key={element.id}></CryptoElement>)}
+            </ul>}
+        </>
     )
 
 }
